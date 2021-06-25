cdd08502b6f0c9c99a3ea3a1a02ef58c51e29ac7
Vedant Jadhav <52625714+YokelMan@users.noreply.github.com>
2021-06-25 23:37:26 +0530

    Update Main.java
    Showed better example in main method.

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Create objects and call method here! An example is shown below:
         CaesarCipher cc = new CaesarCipher();
-        System.out.println(cc.encode("i love coding!", 4));
-        System.out.println(cc.decode("fvyl", 4));
+        System.out.println(cc.encode("exampleTest", 4));
+        System.out.println(cc.decode("ibeqtpixiwx", 4));
     }
 }
