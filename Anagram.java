4317d120de13bde27cd2494e74112d93d1d0dc7d
Vedant Jadhav <52625714+YokelMan@users.noreply.github.com>
2021-06-25 16:16:48 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,24 @@
+package Vedant.Java_Projects;
+
+public class Anagram {
+    void check(String str1, String str2) {
+        char str1Copy[] = str1.toCharArray(), str2Copy[] = str2.toCharArray();
+        if (str1.length() != str2.length()) {
+            System.out.println("The words " + str1 + " and " + str2 + " are not anagrams!");
+            System.exit(0);
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1Copy[i] == '-' || str2Copy[j] == '-')
+                    continue;
+                if (str1Copy[i] == str2Copy[j]) {
+                    str1Copy[i] = str2Copy[j] = '-';
+                }
+            }
+        }
+        if (String.valueOf(str1Copy).equals("----") && String.valueOf(str2Copy).equals("----")) 
+            System.out.println("The words " + str1 + " and " + str2 + " are anagrams!");
+        else
+            System.out.println("The words " + str1 + " and " + str2 + " are not anagrams!");
+    }
+}
